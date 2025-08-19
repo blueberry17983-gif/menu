60926c7c2629c7f8100cef1cf14ce5fb381c9458
blueberry17983-gif <blueberry17983@gmail.com>
2025-08-19 10:41:49 +0300

    Add Some navbar Features

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
 setTimeout(() => {
   document.getElementById("splash-screen").style.display = "none";
 }, 2500);
+
+// Close Navbar
+document.querySelectorAll(".navbar-nav .nav-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    let navbar = document.querySelector(".navbar-collapse");
+    let bsCollapse = new bootstrap.Collapse(navbar, {
+      toggle: false,
+    });
+    bsCollapse.hide();
+  });
+});
+
+// Active Navbar
+document.querySelectorAll(".navbar-nav .nav-link").forEach((link) => {
+  link.addEventListener("click", function () {
+    document
+      .querySelectorAll(".navbar-nav .nav-link")
+      .forEach((nav) => nav.classList.remove("active"));
+    this.classList.add("active");
+    const navbarCollapse = document.querySelector(".navbar-collapse");
+    if (navbarCollapse.classList.contains("show")) {
+      new bootstrap.Collapse(navbarCollapse).toggle();
+    }
+  });
+});
