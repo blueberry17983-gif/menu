92ee8b3ff472bda956ba889a0aed0769f5c0bb29
blueberry17983-gif <blueberry17983@gmail.com>
2025-08-19 11:25:00 +0300

    Edit Some Touches

--- js/index.js
+++ js/index.js
@@ -26,3 +26,24 @@ document.querySelectorAll(".navbar-nav .nav-link").forEach((link) => {
     }
   });
 });
+
+// Back To Top
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  const btn = document.getElementById("backToTop");
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+}
+
+document.getElementById("backToTop").addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
